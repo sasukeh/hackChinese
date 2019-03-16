bea4b9de035a63feaa278dd49529432fa992b241
Yuki Hattori <yuki.hattori@outlook.com>
2019-03-16 14:13:51 +0900

    change extension from tsv to csv

--- letsHackChinese.py
+++ letsHackChinese.py
@@ -1,19 +1,17 @@
 # -*- coding: utf-8 -*-
 from pprint import pprint 
-import re
 import csv
-import os
-
-# pathObjectToJson
-def jsonFileToDict(file):
-  f = open(file)
-  return json.load(f)
 
 def csvFileToCsvObject(file, readerType=""):
-  f = open(file, encoding='utf_8_sig' ) 
+  f = open(file, "r") 
   if readerType=="dict":
     return csv.DictReader(f)
   else:
     return csv.reader(f)
 
+csv = csvFileToCsvObject("./pinYinTable.csv")
+# for a in tsv:
+#   pprint(a)
+for a in csv:
+  pprint(a)
 
