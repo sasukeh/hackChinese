2aa5957673c87db9d2288ef21275fb3f4055fbbc
Yuki Hattori <yuki.hattori@outlook.com>
2019-03-16 17:01:09 +0900

    add

--- letsHackChinese.py
+++ letsHackChinese.py
@@ -1,6 +1,52 @@
 # -*- coding: utf-8 -*-
 from pprint import pprint 
+import numpy as np
 import pandas
+import unidecode
+import re
 
-csv = pandas.read_csv("./pinYinTable.csv", encoding='utf-16-le')
-pprint(csv)
+csv = pandas.read_csv("./masterSentences.csv", encoding='utf-16-le')
+
+## all PinYin in sentences
+msPinyins= []
+for sentence in csv["Pronunciation2"]:
+    for pronunciation in re.split(" |\.|\?", sentence):
+        basePronunciation = unidecode.unidecode(pronunciation).lower()
+        if basePronunciation != "" and basePronunciation not in msPinyins: 
+            msPinyins.append(basePronunciation)
+
+
+# all PinYin from PinYin Table
+pinYinList = pandas.read_csv("./pinYinTable.csv", encoding='utf-16-le')
+
+# pinyin coverage dict
+masterInitialDict = {}
+for initial in pinYinList.columns:
+    masterInitialDict[initial] = 0
+masterFinalDict = {}
+for final in pinYinList.iloc[:,0]:
+    masterFinalDict[final] = 0
+
+# get finals and initials count
+pinYinList.set_index('index', inplace=True)
+for py in msPinyins:
+    for word in pinYinList:
+        r = pinYinList[word][pinYinList[word] == py]
+        if not r.empty:
+            initial = word
+            final = r.index[0]
+            masterFinalDict[final] = masterFinalDict[final] + 1
+            masterInitialDict[initial] = masterInitialDict[initial] + 1
+
+finalsNeverUsed = [k for k, v in masterFinalDict.items() if v == 0]
+initialsNeverUsed = [k for k, v in masterInitialDict.items() if v == 0]
+
+
+# print results
+print("DONE: Finals you used : %i /%i (%f)"%(len(masterFinalDict.keys())-len(finalsNeverUsed), len(masterFinalDict.keys()), 100-len(finalsNeverUsed)*1.0/len(masterFinalDict.keys())*100))
+print("Let's use Finals below!")
+print(finalsNeverUsed)
+
+print("\nDONE: Initials you used : %i /%i (%f)"%(len(masterInitialDict.keys())-len(initialsNeverUsed), len(masterInitialDict.keys()), 100-len(initialsNeverUsed)*1.0/len(masterInitialDict.keys())*100))
+print("Let's use initials below!")
+print(initialsNeverUsed)
