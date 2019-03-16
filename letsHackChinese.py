3dcbcf0bfc1d4dff48b033858546180ef7a49554
Yuki Hattori <yuki.hattori@outlook.com>
2019-03-16 14:26:40 +0900

    change csv to pandas

--- letsHackChinese.py
+++ letsHackChinese.py
@@ -1,17 +1,6 @@
 # -*- coding: utf-8 -*-
 from pprint import pprint 
-import csv
+import pandas
 
-def csvFileToCsvObject(file, readerType=""):
-  f = open(file, "r") 
-  if readerType=="dict":
-    return csv.DictReader(f)
-  else:
-    return csv.reader(f)
-
-csv = csvFileToCsvObject("./pinYinTable.csv")
-# for a in tsv:
-#   pprint(a)
-for a in csv:
-  pprint(a)
-
+csv = pandas.read_csv("./pinYinTable.csv", encoding='utf-16-le')
+pprint(csv)
